e1724b782ebf8b9430c704e9799c8593077cce96
Bruno Brizolara <bbrizolara7@gmail.com>
2024-11-17 17:00:20 -0300

    initial configuration and theme and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+  </>
+);
 
-export default function Home() {
-  return (
-
-  );
-}
+export default Home;
